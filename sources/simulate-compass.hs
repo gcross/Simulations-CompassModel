99f3de1b405cc21dddf982d14247c9190572d6ef
Gregory Crosswhite <gcross@phys.washington.edu>
2010-01-30 20:00:39 -0800

    Reworked Compass Hamiltonian to use new style.

--- sources/simulate-compass.hs
+++ sources/simulate-compass.hs
@@ -1,17 +1,18 @@
 -- @+leo-ver=4-thin
--- @+node:gcross.20100128100529.1239:@thin simulate-compass.hs
+-- @+node:gcross.20100130190931.1265:@thin simulate-compass.hs
 -- @@language Haskell
 
 -- @<< Language extensions >>
--- @+node:gcross.20091120111528.1238:<< Language extensions >>
+-- @+node:gcross.20100130190931.1266:<< Language extensions >>
 {-# LANGUAGE ScopedTypeVariables #-}
--- @-node:gcross.20091120111528.1238:<< Language extensions >>
+-- @-node:gcross.20100130190931.1266:<< Language extensions >>
 -- @nl
 
 -- @<< Import needed modules >>
--- @+node:gcross.20091120111528.1235:<< Import needed modules >>
+-- @+node:gcross.20100130190931.1267:<< Import needed modules >>
 import Acme.Dont
 
+import Control.Applicative.Infix
 import Control.Monad
 import Control.Monad.Error
 import Control.Monad.Trans
@@ -36,88 +37,127 @@ import VMPS.States
 import VMPS.Tensors
 
 import Debug.Trace
--- @-node:gcross.20091120111528.1235:<< Import needed modules >>
+-- @-node:gcross.20100130190931.1267:<< Import needed modules >>
 -- @nl
 
 -- @+others
--- @+node:gcross.20091120111528.1234:Operator tensors
+-- @+node:gcross.20100130190931.1274:Operator tensors
 makeModelOperatorSiteTensors :: Double -> Int -> Int -> [OperatorSiteTensor]
-makeModelOperatorSiteTensors angle width height =
-    let makeRow left middle right =
-            [left] ++ replicate (width-2) middle ++ [right]
+makeModelOperatorSiteTensors factor width height =
+    let --angle = factor * pi/2
+        --x_factor = cos angle
+        --z_factor = sin angle
+        z_factor = factor
+        x_factor = 1-factor
 
-        width_ = fromIntegral width
+        x_scale = (-x_factor) :+ 0
+        z_scale = (-z_factor) :+ 0
 
-        bandwidth = (4+width-1)
+        horizontal_obc = 3
 
-        no_op =
-            [(1 --> 1) pI
-            ,(2 --> 2) pI
+        vertical_obc_start = 4
+        vertical_obc_range = [vertical_obc_start..vertical_obc_start+width-1]
+
+        bandwidth = maximum
+            [horizontal_obc
+            ,last vertical_obc_range
             ]
 
-        start_horizontal =
-            [(1 --> 3) ((cos angle :+ 0) *: pX)]
-        end_horizontal =
-            [(3 --> 2) pX]
+        passRange = concat . map pass
 
-        start_vertical =
-            [(1 --> 4) ((sin angle :+ 0) *: pZ)]
-        run_vertical =
-            [(i --> (i+1)) pI | i <- [4..4+width_-2]]
-        end_vertical =
-            [((4+width_-1) --> 2) pZ]
+        pass index =
+            [(index_ --> index_) pI]
+          where
+            index_ = fromIntegral index
 
-        initiate =
-            [(1 --> 1) pI] ++ start_horizontal ++ start_vertical
+        throw operator index =
+            [(1 --> fromIntegral index) operator]
+        catch operator index =
+            [(fromIntegral index --> 2) operator]
 
-        terminus =
-            [(2 --> 1) pI
-            ,(3 --> 1) pX
-            ,((4+width_-1) --> 1) pZ]
+        throwHorizontal = throw (x_scale *: pX)
+        catchHorizontal = catch pX
 
-        middle_horizontal =
-            start_horizontal ++ end_horizontal
-        middle_vertical =
-            start_vertical ++ run_vertical ++ end_vertical
+        throw_horizontal_obc = throwHorizontal horizontal_obc
+        catch_horizontal_obc = catchHorizontal horizontal_obc
+        throw_or_catch_horizontal_obc = throw_horizontal_obc ++ catch_horizontal_obc
 
-        top_vertical = start_vertical ++ run_vertical
-        bottom_vertical = run_vertical ++ end_vertical
+        throwVertical = throw (z_scale *: pZ)
+        catchVertical = catch pZ
 
-        top_left = makeOperatorSiteTensorFromSpecification 1 bandwidth $
-            initiate
-        top_middle = makeOperatorSiteTensorFromSpecification bandwidth bandwidth $
-            no_op ++ middle_horizontal ++ top_vertical
-        top_right = makeOperatorSiteTensorFromSpecification bandwidth bandwidth $
-            no_op ++ end_horizontal ++ top_vertical
-        top = makeRow top_left top_middle top_right
+        throwVerticalOBC = throwVertical . (vertical_obc_start +)
+        catchVerticalOBC = catchVertical . (vertical_obc_start +)
+        throwOrCatchVerticalOBC = throwVerticalOBC <^(++)^> catchVerticalOBC
 
-        middle_left = makeOperatorSiteTensorFromSpecification bandwidth bandwidth $
-            no_op ++ start_horizontal ++ middle_vertical
-        middle_middle = makeOperatorSiteTensorFromSpecification bandwidth bandwidth $
-            no_op ++ middle_horizontal ++ middle_vertical
-        middle_right = makeOperatorSiteTensorFromSpecification bandwidth bandwidth $
-            no_op ++ end_horizontal ++ middle_vertical
-        middle = makeRow middle_left middle_middle middle_right
+        passVerticalOBCRange = passRange . map (vertical_obc_start +)
+        passThrownVerticalOBC column = passVerticalOBCRange [0..column-1]
+        passCaughtVerticalOBC column = passVerticalOBCRange [column+1..width-1]
+        passThrownAndCaughtVerticalOBC = passThrownVerticalOBC <^(++)^> passCaughtVerticalOBC
 
-        bottom_left = makeOperatorSiteTensorFromSpecification bandwidth bandwidth $
-            no_op ++ start_horizontal ++ bottom_vertical
-        bottom_middle = makeOperatorSiteTensorFromSpecification bandwidth bandwidth $
-            no_op ++ middle_horizontal ++ bottom_vertical
-        bottom_right = makeOperatorSiteTensorFromSpecification bandwidth 1 $
-            terminus
-        bottom = makeRow bottom_left bottom_middle bottom_right
+        top =
+            [makeOperatorSiteTensorFromSpecification 1 bandwidth $
+             pass 1
+                ++ throw_horizontal_obc ++ throwVerticalOBC 0
+            ]
+            ++
+            [makeOperatorSiteTensorFromSpecification bandwidth bandwidth $
+             passRange [1,2] ++ passThrownVerticalOBC column
+                ++ throw_or_catch_horizontal_obc ++ throwVerticalOBC column
+            | column <- [1..width-2]
+            ]
+            ++
+            [makeOperatorSiteTensorFromSpecification bandwidth bandwidth $
+             passRange [1,2] ++ passThrownVerticalOBC (width-1)
+                ++ catch_horizontal_obc ++ throwVerticalOBC (width-1)
+            ]
+
+        middle =
+            [makeOperatorSiteTensorFromSpecification bandwidth bandwidth $
+             passRange [1,2] ++ passThrownAndCaughtVerticalOBC 0
+                ++ throw_horizontal_obc ++ throwOrCatchVerticalOBC 0
+            ]
+            ++
+            [makeOperatorSiteTensorFromSpecification bandwidth bandwidth $
+             passRange [1,2] ++ passThrownAndCaughtVerticalOBC column
+                ++ throw_or_catch_horizontal_obc ++ throwOrCatchVerticalOBC column
+            | column <- [1..width-2]
+            ]
+            ++
+            [makeOperatorSiteTensorFromSpecification bandwidth bandwidth $
+             passRange [1,2] ++ passThrownAndCaughtVerticalOBC (width-1)
+                ++ catch_horizontal_obc ++ throwOrCatchVerticalOBC (width-1)
+            ]
+
+        bottom =
+            [makeOperatorSiteTensorFromSpecification bandwidth bandwidth $
+             passRange [1,2] ++ passCaughtVerticalOBC 0
+                ++ throw_horizontal_obc ++ catchVerticalOBC 0
+            ]
+            ++
+            [makeOperatorSiteTensorFromSpecification bandwidth bandwidth $
+             passRange [1,2] ++ passCaughtVerticalOBC column
+                ++ throw_or_catch_horizontal_obc ++ catchVerticalOBC column
+            | column <- [1..width-2]
+            ]
+            ++
+            [makeOperatorSiteTensorFromSpecification bandwidth 1 $
+                [(2 --> 1) pI
+                ,(3 --> 1) pX
+                ,((fromIntegral . last) vertical_obc_range --> 1) pZ
+                ]
+            ]
 
     in top ++ (concat . replicate (height-2)) middle ++ bottom
--- @-node:gcross.20091120111528.1234:Operator tensors
--- @+node:gcross.20091120111528.1237:analyzeTrialEnergies
+-- @-node:gcross.20100130190931.1274:Operator tensors
+-- @+node:gcross.20100130190931.1269:analyzeTrialEnergies
 data TrialAnalysis = TrialDidBetter | TrialDidWorse | TrialDidTheSame
 
 analyzeTrialEnergies tolerance best_energy trial_energy
     | best_energy - trial_energy > tolerance = TrialDidBetter
     | trial_energy - best_energy > tolerance = TrialDidWorse
     | otherwise = TrialDidTheSame
--- @-node:gcross.20091120111528.1237:analyzeTrialEnergies
--- @+node:gcross.20091120111528.1236:main
+-- @-node:gcross.20100130190931.1269:analyzeTrialEnergies
+-- @+node:gcross.20100130190931.1270:main
 main = do
     args <- getArgs
 
@@ -127,8 +167,8 @@ main = do
         operator_site_tensors = makeModelOperatorSiteTensors angle width height
         bandwidth_increment = 5
         initial_bandwidth = 2
-        bandwidth_increase_energy_change_convergence_criterion = 1e-4
-        multisweep_energy_change_convergence_criterion = 1e-4
+        bandwidth_increase_energy_change_convergence_criterion = 1e-2
+        multisweep_energy_change_convergence_criterion = 1e-2
         level_similarity_tolerance = 1e-3
 
     putStrLn $ "Angle = " ++ show angle
@@ -137,7 +177,7 @@ main = do
     putStrLn $ "Total = " ++ show (length operator_site_tensors)
 
     -- @    << Define callbacks >>
-    -- @+node:gcross.20091205211300.1710:<< Define callbacks >>
+    -- @+node:gcross.20100130190931.1271:<< Define callbacks >>
     next_bandwidth_ref <- newIORef initial_bandwidth
     level_number_ref <- newIORef 1
 
@@ -160,11 +200,11 @@ main = do
             let current_bandwidth = next_bandwidth-bandwidth_increment
             unless (current_bandwidth <= 2) $
                 putStrLn $ heading ++ (printf "bandwidth = %i, sweep energy = %f" current_bandwidth (chainEnergy latest_chain) )
-    -- @-node:gcross.20091205211300.1710:<< Define callbacks >>
+    -- @-node:gcross.20100130190931.1271:<< Define callbacks >>
     -- @nl
 
     -- @    << Run simulation >>
-    -- @+node:gcross.20091202133456.1303:<< Run simulation >>
+    -- @+node:gcross.20100130190931.1272:<< Run simulation >>
     (energies,_,_) <- fmap unzip3 $
         solveForMultipleLevelsWithCallbacks
             callback_to_decide_whether_to_declare_victory_with_trial
@@ -180,9 +220,9 @@ main = do
             multisweep_energy_change_convergence_criterion
             0
             1000
-            2
+            3
             []
-    -- @-node:gcross.20091202133456.1303:<< Run simulation >>
+    -- @-node:gcross.20100130190931.1272:<< Run simulation >>
     -- @nl
 
     putStrLn ""
@@ -195,7 +235,7 @@ main = do
 
     putStrLn $ "The elapsed CPU time for this run was " ++ show time_in_seconds ++ " seconds."
 
--- @-node:gcross.20091120111528.1236:main
+-- @-node:gcross.20100130190931.1270:main
 -- @-others
--- @-node:gcross.20100128100529.1239:@thin simulate-compass.hs
+-- @-node:gcross.20100130190931.1265:@thin simulate-compass.hs
 -- @-leo
